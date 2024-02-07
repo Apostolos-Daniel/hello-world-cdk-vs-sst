2251c77e09957b27e92ae7bcb59868a202b3435f
Toli <toli@flipdish.com>
2024-02-07 12:07:06 +0000

    remove all other apps, stick to an sst app

--- my-node-app/helpers/observability/index.ts
+++
@@ -1,6 +0,0 @@
-import { tracer } from 'dd-trace';
-tracer.init();
-
-import { setTag } from './set-tag';
-
-export { setTag };
--- my-sst-app/packages/functions/src/lambda.ts
+++ my-sst-app/packages/functions/src/lambda.ts
@@ -1,8 +1,17 @@
 import { ApiHandler } from "sst/node/api";
+import { datadog } from "datadog-lambda-js";
+import tracer from "dd-trace";
+tracer.init();
 
-export const handler = ApiHandler(async (_evt) => {
+export const handler = datadog(ApiHandler(async (_evt) => {
+  const span = tracer?.scope().active();
+  if (span) {
+    console.log('Setting tag');
+    span.setTag('foo', 'bar');
+  }
   return {
     statusCode: 200,
     body: `Hello world. The time is ${new Date().toISOString()}`,
   };
-});
+}));
+ 
